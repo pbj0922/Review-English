999a326bd86d9da372a290637f0f266e111225d4
ATTIPARK <qkrqjawls0922@gmail.com>
2023-09-05 00:40:24 +0900

    버튼에 함수 추가하기

--- src/app/day/[id]/page.tsx
+++ src/app/day/[id]/page.tsx
@@ -20,6 +20,7 @@ const Day: NextPage = () => {
   const { id } = useParams();
 
   const [review, setReview] = useState<Treview>();
+  const [currentReviewIndex, setCurrentReviewIndex] = useState<number>(0);
 
   useEffect(() => {
     if (typeof id !== "string") return;
@@ -32,12 +33,18 @@ const Day: NextPage = () => {
       <div className="w-full relative flex justify-center">
         <div className="absolute top-0 left-0">
           <Link href="/">
-            <button className="btn-style">Back</button>
+            <button className="btn-style text-xs font-semibold">Back</button>
           </Link>
         </div>
         <div className="font-semibold mb-24 mt-2">Day {id}</div>
       </div>
-      <ReviewCard />
+      {review && (
+        <ReviewCard
+          sentences={review.sentences}
+          currentReviewIndex={currentReviewIndex}
+          setCurrentReviewIndex={setCurrentReviewIndex}
+        />
+      )}
     </main>
   );
 };
--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,15 +1,55 @@
-import { FC } from "react";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 
 import LeftArrow from "@/app/icons/LeftArrow";
 import RightArrow from "@/app/icons/RightArrow";
 import Speaker from "@/app/icons/Speaker";
+import { Treview } from "@/app/day/[id]/page";
 
-const ReviewCard: FC = () => {
+type ReviewCardProps = Pick<Treview, "sentences"> & {
+  currentReviewIndex: number;
+  setCurrentReviewIndex: Dispatch<SetStateAction<number>>;
+};
+
+type TLanguage = "korean" | "english";
+
+const ReviewCard: FC<ReviewCardProps> = ({
+  sentences,
+  currentReviewIndex,
+  setCurrentReviewIndex,
+}) => {
+  const [language, setLanguage] = useState<TLanguage>("korean");
+
+  const onClickLanguage = () => {
+    if (language === "korean") {
+      setLanguage("english");
+    } else {
+      setLanguage("korean");
+    }
+  };
+
+  const onClickPrev = () => {
+    if (currentReviewIndex <= 0) {
+      setCurrentReviewIndex(sentences.length - 1);
+    } else {
+      setCurrentReviewIndex(currentReviewIndex - 1);
+    }
+    setLanguage("korean");
+  };
+
+  const onClickNext = () => {
+    if (currentReviewIndex >= sentences.length - 1) {
+      setCurrentReviewIndex(0);
+    } else {
+      setCurrentReviewIndex(currentReviewIndex + 1);
+    }
+    setLanguage("korean");
+  };
+
   return (
     <div className="w-full">
       <div className="h-60">
         <div className="border-black border-2 px-4 py-2">
-          Working from home isn't for me. I always get distracted.
+          {sentences[currentReviewIndex][language]}
         </div>
         <div className="mt-2">
           <button className="btn-style">
@@ -18,14 +58,19 @@ const ReviewCard: FC = () => {
         </div>
       </div>
       <div className="mt-4 flex justify-between">
-        <button className="btn-style">
+        <button onClick={onClickPrev} className="btn-style">
           <LeftArrow />
         </button>
-        <button className="btn-style">
-          <span className="mr-1">KOR</span>/
-          <span className="ml-1  font-semibold">ENG</span>
+        <button onClick={onClickLanguage} className="btn-style">
+          <span className={`${language === "korean" && "font-semibold"} mr-1`}>
+            KOR
+          </span>
+          /
+          <span className={`${language === "english" && "font-semibold"} ml-1`}>
+            ENG
+          </span>
         </button>
-        <button className="btn-style">
+        <button onClick={onClickNext} className="btn-style">
           <RightArrow />
         </button>
       </div>
